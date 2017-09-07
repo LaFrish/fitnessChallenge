124e8f44bbeb22ad3fdb12f675fc437e54e07b21
YOUR FULL NAME <farishta.haider@gmail.com>
2017-09-06 23:08:15 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :goals
+    resources :logs
+  end
+
 end
